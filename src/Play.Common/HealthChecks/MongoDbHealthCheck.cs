21496dd57988d3d210d3110c83457bf73221e6e6
Wando Wagner Schneider <wandoschneider@gmail.com>
2022-11-05 12:43:38 -0300

    removendo log de teste

--- src/Play.Common/HealthChecks/MongoDbHealthCheck.cs
+++ src/Play.Common/HealthChecks/MongoDbHealthCheck.cs
@@ -1,7 +1,6 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
-using Microsoft.Extensions.Logging;
 using MongoDB.Driver;
 
 namespace Play.Common.HealthChecks;
@@ -9,13 +8,10 @@ namespace Play.Common.HealthChecks;
 public class MongoDbHealthCheck : IHealthCheck
 {
     private readonly MongoClient client;
-    private readonly ILogger<MongoDbHealthCheck> logger;
-
 
     public MongoDbHealthCheck(MongoClient client)
     {
         this.client = client;
-        System.Console.WriteLine("teste client" + client.ToString());
     }
 
     public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
