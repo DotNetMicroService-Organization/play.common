0e78ff254939acf50b1bf7925b2241a4b0fecf44
Wando Wagner Schneider <wandoschneider@gmail.com>
2022-11-05 12:20:42 -0300

    adicionando log health check

--- src/Play.Common/HealthChecks/MongoDbHealthCheck.cs
+++ src/Play.Common/HealthChecks/MongoDbHealthCheck.cs
@@ -1,6 +1,7 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Logging;
 using MongoDB.Driver;
 
 namespace Play.Common.HealthChecks;
@@ -8,14 +9,20 @@ namespace Play.Common.HealthChecks;
 public class MongoDbHealthCheck : IHealthCheck
 {
     private readonly MongoClient client;
+    private readonly ILogger<MongoDbHealthCheck> logger;
 
-    public MongoDbHealthCheck(MongoClient client)
+
+    public MongoDbHealthCheck(MongoClient client, ILogger<MongoDbHealthCheck> logger)
     {
         this.client = client;
+        this.logger = logger;
     }
 
     public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
     {
+        logger.LogInformation(
+                            "AAAA Receiving mongodbclient {MongoClient}",
+                            this.client);
         try
         {
             await client.ListDatabaseNamesAsync(cancellationToken);
